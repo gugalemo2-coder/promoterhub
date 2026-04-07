18dd18882397f08f7cae6acc7e5fdd7595ee0f7c
gugalemo2-coder <guga.lemo2@gmail.com>
2026-04-07 17:45:32 -0300

    Update web-push-setup.tsx

--- web/components/web-push-setup.tsx
+++ web/components/web-push-setup.tsx
@@ -33,7 +33,7 @@ export function WebPushSetup() {
         if (!subscription) {
           subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as unknown as BufferSource,
           });
         }
 
